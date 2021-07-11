105a18a1bca2cfcc61009c3bc6d547ab748fdeec
Shane Graff <copellius@gmail.com>
2021-07-11 15:14:22 -0700

    Initial commit
    Co-authored-by: Alex Goldberg <alexdominguezwebdevl@gmail.com>
    Co-authored-by: Alex Bertotto <ajb5206@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,53 @@
 //Business Logic
 
-funtion PigDice() {
-  this.players {};
-  this.currentID = 0:
+// function PigDice (player1, player2) {
+//   this.player1 = player1;
+//   this.player2 = player2;
+// }
+
+function PigDice() {
+  this.players = {};
+  this.currentId = 0;
 }
 
+PigDice.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+PigDice.prototype.addPlayer = function(player) {
+  player.id = this.assignId();
+  this.players[player.id] = player;
+};
+
+function Player(currentScore) {
+  this.currentScore = currentScore;
+  return this.currentScore;
+}
+
+function Die() {
+  return Math.floor((Math.random() * 6) + 1);
+}
+
+function Turn () {
+  // hit button to roll
+  let turnScore = 0;
+  let roll = Die();
+
+  if (roll === 1) {
+    this.roll = 0;
+    //switch player
+  } else {
+    turnScore = this.roll += roll;
+    //store value in turnScore
+  }
+  return turnScore;
+}
+
+function Hold() {
+// save turnScore to currentScore
+// switch player
+}
 
 
 //UserInterface Logic
@@ -13,6 +56,5 @@ $(document).ready(function() {
     event.preventDefault();
 
 
-    
   });
 });
