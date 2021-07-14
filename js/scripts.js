30513b3246c8d0a9dec5269ea194aeedbff05b3b
Shane Graff <copellius@gmail.com>
2021-07-13 21:08:23 -0700

    Change Player class
    Co-authored-by: Alex Bertotto <ajb5206@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,11 @@
 
 let turnScore = 0;
 
-function PigDice() {
-  this.players = {};
-  this.currentId = 0;
-}
+function PigDice () {
+    this.players = {};
+    this.currentId = 0;
+    this.turnId = 1;
+  }
 
 PigDice.prototype.assignId = function() {
   this.currentId += 1;
@@ -17,27 +18,45 @@ PigDice.prototype.addPlayer = function(player) {
   this.players[player.id] = player;
 };
 
-function Player(currentScore) {
-  this.currentScore = turnScore + currentScore;
-  return this.currentScore;
+PigDice.prototype.changePlayer() {
+  const numPlayers = this.players.length;
+  if (this.turnId > numPlayers) {
+    this.turnId = 1;
+  } else {
+    this.turnId += 1;
+//    this.players[this.turnId].score;
+  }
+
 }
 
-function Die() {
+function Player() {
+  this.score = 0;
+}
+
+Player.prototype.addScore = function(currentScore){
+  //add logic
+  
+}
+
+// die!
+function die() {
   return Math.floor((Math.random() * 6) + 1);
 }
 
 function PlayerSelect() {
+  // pigDice.players[]
 
 }
+
 
 function Turn () {
   // hit button to roll
   //let turnScore = 0;
   //let roll = Die();
   let turnScore = 0;
-
+  turnScore += rollDice;
   if (roll === 1) {
-    this.roll = 0;
+    this.turnScore = 0;
     //switch player
   } else {
     //turnScore = this.roll += roll;
@@ -51,19 +70,38 @@ function Hold() {
 // switch player
 }
 
+function Player(currentScore) {
+  this.currentScore = 0;
+}
+
 //UserInterface Logic
 $(document).ready(function() {
-  $("form#language").submit(function(event) {
+  let pigDice = new PigDice();
+  let newPlayer = new Player();
+  let newPlayer1 = new Player();
+  
+  $("form#roll").submit(function(event) {
     event.preventDefault();
-    $("#score1") = turnScore;
-    let rollDice = Die();
+    // $("#score1") = turnScore;
+    let rollDice = die();
     console.log(rollDice);
+    $("#rollScore").text(rollDice);
+
+    pigDice.addPlayer(newPlayer);
+    pigDice.addPlayer(newPlayer1);
+    console.log(pigDice);
 
   //  $("button#rollDice").click(function() {
   //    let roll = Die();
   //    console.log(roll);
-      //turnScore = roll += roll;
-  //  });
+      // turnScore = roll += roll;
+  // });
+  // });
+  // $("form#startGame").submit(function(event) {
+  //   event.preventDefault();
+  //   // let pigDice = new PigDice();
+  //   console.log("1");
+    
   });
 });
 
